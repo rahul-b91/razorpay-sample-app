535a946f7440bdbc777316c7bbeed7c3403aa0b4
rahul-b91 <rahul.rohilla@banc91.com>
2021-07-01 23:15:02 +0530

    added vuejs functionality for razorpay

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,100 +1,59 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="col-md-12">
-        <form>
-          <div class="form-group">
-            <label>Name</label>
-            <input v-model="name" type="text" class="form-control" />
-          </div>
-          <div class="form-group">
-            <label>Description</label>
-            <input v-model="desc" type="text" class="form-control" />
-          </div>
-
-          <div class="form-group">
-            <label>Price</label>
-            <input v-model="price" type="number" class="form-control" />
-          </div>
-
-          <button @click="makePayment" class="btn btn-success">Submit</button>
-        </form>
-      </div>
+      <button @click="makePayment">Pay</button>
     </div>
   </div>
 </template>
 <script>
-import axios from "axios";
 export default {
-  name: "home",
-  components: {},
   data() {
-    return {
-      price: 0,
-      order_id: "",
-      name: "",
-      desc: ""
-    };
+    return {};
   },
   methods: {
-    //Function to create order ID
-    makePayment: function(ev) {
-      ev.preventDefault();
-      var data = {};
-      (data["name"] = this.name),
-        (data["desc"] = this.desc),
-        (data["amount"] = 500),
-        axios({
-          method: "post",
-          url: "http://YOUR URL TO CREATE ORDER ID/",
-          data: data,
-        })
-          .then(res => {
-            this.order_id = res.data.order_id;
-            //Initiation of Razorpay PG
-            var rzp1 = new Razorpay({
-              key: "--RAZORPAY KEY--",
-              amount: res.data.amount,
-              name: res.data.name,
-              currency: "INR",
-              description: res.data.desc,
-              image: "Link Of Image",
+    makePayment(e) {
+      let rzp1 = new Razorpay(this.createObj());
+      rzp1.on("payment.failed", function (response) {
+        alert(response.error.code);
+        alert(response.error.description);
+        alert(response.error.source);
+        alert(response.error.step);
+        alert(response.error.reason);
+        alert(response.error.metadata.order_id);
+        alert(response.error.metadata.payment_id);
+      });
 
-              //Uncomment if you are using handler function
-
-              // "handler": function (response){
-              //   alert(response.razorpay_payment_id);
-              // },
-              //callback_url: 'http://13.126.183.214/razorpay/checkstatus/?orderid='+this.order_id,
-
-              prefill: {
-                name: "Allen Shaji",
-                email: "allenshaji10@gmail.com"
-              },
-
-              notes: {
-                address: ""
-              },
-
-              theme: {
-                color: "#00ffff"
-              },
-              order_id: res.data.order_id,
-              callback_url: "http://domain.com/#/about/" + this.order_id,
-              redirect: true
-            });
-            rzp1.open();
-          })
-          .catch(err => {
-            console.log("ERR", err);
-          });
-    }
-    //if you are using handler function
-
-    // verify: function(response) {
-
-    // }
-  }
+      rzp1.open();
+      e.preventDefault();
+    },
+    createObj() {
+      return {
+        key: "rzp_test_buSe3lz98IWBl7", // Enter the Key ID generated from the Dashboard
+        // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+        amount: "5000",
+        name: "card91",
+        description: "Pay to card91",
+        image: "https://i.ibb.co/rtvCt6d/cardgf.png",
+        order_id: "order_HTdVtD0tJCQDUF", //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
+        handler: function (response) {
+          alert(response.razorpay_payment_id);
+          alert(response.razorpay_order_id);
+          alert(response.razorpay_signature);
+        },
+        prefill: {
+          name: "Gaurav Kumar",
+          email: "gaurav.kumar@example.com",
+          contact: "9999999999",
+        },
+        notes: {
+          address: "Razorpay Corporate Office",
+        },
+        theme: {
+          color: "#3399cc",
+        },
+      };
+    },
+  },
 };
 </script>
 
